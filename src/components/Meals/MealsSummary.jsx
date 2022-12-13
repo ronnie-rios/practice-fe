76be3bab013fc7fc8b76324d4b8bacc9cb843809
Ronnie Rios <ronniesmail14@gmail.com>
2022-12-13 12:10:18 -0600

    mealsummary component

---
+++ src/components/Meals/MealsSummary.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import classes from './MealSummary.module.css';
+
+export default function MealsSummary() {
+  return (
+    <section className={classes.summary}>
+        <h2>food stuffs</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia sed error unde suscipit nobis quam possimus iure fugit maxime voluptates!</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia sed error unde suscipit nobis quam possimus iure fugit maxime voluptates!</p>
+    </section>
+  )
+}
