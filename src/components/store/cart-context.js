2f0341955df4c160fb27d768ce52f5730672ffa2
Ronnie Rios <ronniesmail14@gmail.com>
2022-12-16 16:09:06 -0600

    cart reducer

--- src/components/store/cart-context.js
+++ src/components/store/cart-context.js
@@ -1,4 +1,23 @@
-import { createContext } from 'react';
+import { useState, useReducer, createContext } from 'react';
+
+const initialCartState = {
+    items: [],
+    totalAmount: 0
+}
+
+const cartReducer = (state, action) => {
+    if (type ==='ADD_TO_CART') {
+        const updatedCart = state.items.concat(action.payload);
+        const updatedTotal = state.totalAmount + action.payload.price * action.payload.amount
+        return {
+            items: updatedCart,
+            totalAmount: updatedTotal
+        }
+    } else if (type === 'REMOVE_FROM_CART'){
+
+    }
+    return initialCartState
+}
 
 export const CartContext = createContext({
     items: [],
@@ -9,11 +28,13 @@ export const CartContext = createContext({
 
 //PROVIDER
 export const CartProvider = props => {
+   const [cartState, dispatchCart] = useReducer(cartReducer, initialCartState);
     
-    const addItemToCart = (items) => {
-
+    const addItemToCart = (item) => {
+        dispatchCart({ type: 'ADD_TO_CART', payload: item })
     }
     const removeItemFromCart = (id) => {
+        dispatchCart({ type: 'REMOVE_FROM_CART', id: id })
 
     }
     
